4326c1a3ad3c7227fb5db37ec2f9c369ad6a9b1f
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2025-01-05 00:24:21 +0900

    会話が多重起動しないように修正

--- main.gd
+++ main.gd
@@ -5,13 +5,15 @@ const Balloon = preload("res://dialogue/balloon.tscn")
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
 
-func _on_mao_touched() -> void:
-	var balloon: Node = Balloon.instantiate()
-	get_tree().current_scene.add_child(balloon)
-	balloon.dialogue_finished.connect(_on_balloon_dialogue_finished)
-	balloon.start(dialogue_resource, dialogue_start)
-	
+func _on_mao_touched() -> void:	
 	$StateChart.send_event("dialogue_started")
 
 func _on_balloon_dialogue_finished() -> void:
 	$StateChart.send_event("dialogue_finished")
+
+
+func _on_chatting_state_entered() -> void:
+	var balloon: Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.dialogue_finished.connect(_on_balloon_dialogue_finished)
+	balloon.start(dialogue_resource, dialogue_start)
