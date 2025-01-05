70031d6bf2017ad7a1c3635dc571446ba523b4ef
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2025-01-05 15:26:16 +0900

    まおを画面左に

--- mao.gd
+++ mao.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Sprite2D
 
 signal touched
 
@@ -16,32 +16,32 @@ func _process(delta: float) -> void:
 	var texture_height = floor(vct_resolution.y / RENDER_SIZE_STEP) * RENDER_SIZE_STEP
 	texture_height = clampi(texture_height, MIX_RENDER_SIZE, MAX_RENDER_SIZE)
 	
-	$Sprite2D/GDCubismUserModel.size = Vector2.ONE * texture_height
+	$GDCubismUserModel.size = Vector2.ONE * texture_height
 	
 	var vct_viewport_size = Vector2(get_viewport_rect().size)
-	$Sprite2D.position = vct_viewport_size / 2
-	$Sprite2D.scale.x = vct_viewport_size.y / $Sprite2D/GDCubismUserModel.size.y
-	$Sprite2D.scale.y = $Sprite2D.scale.x
+	#position = vct_viewport_size / 2
+	scale.x = vct_viewport_size.y / $GDCubismUserModel.size.y
+	scale.y = scale.x
 	
-	if $Sprite2D.centered == true:
-		adjust_pos = ($Sprite2D/GDCubismUserModel.size * 0.5)
+	if centered == true:
+		adjust_pos = ($GDCubismUserModel.size * 0.5)
 
 	if pressed == true:
-		$Sprite2D/GDCubismUserModel/GDCubismEffectHitArea.set_target(local_pos)
+		$GDCubismUserModel/GDCubismEffectHitArea.set_target(local_pos)
 
 func _input(event):
 	if event as InputEventMouseMotion:
-		local_pos = $Sprite2D.to_local(event.position) + adjust_pos
+		local_pos = to_local(event.position) + adjust_pos
 
 		# マウス座標を表示に使用している Node に変換
-		var pos = $Sprite2D.to_local(event.position)
+		var pos = to_local(event.position)
 		# 変換した座標を SubViewport の表示サイズに調整
 		var render_size: Vector2 = Vector2(
-			float($Sprite2D/GDCubismUserModel.size.x) * $Sprite2D.scale.x,
-			float($Sprite2D/GDCubismUserModel.size.y) * $Sprite2D.scale.y * -1.0
+			float($GDCubismUserModel.size.x) * scale.x,
+			float($GDCubismUserModel.size.y) * scale.y * -1.0
 			) * 0.5
 		pos /= render_size
-		$Sprite2D/GDCubismUserModel/GDCubismEffectTargetPoint.set_target(pos)
+		$GDCubismUserModel/GDCubismEffectTargetPoint.set_target(pos)
 		
 	if event as InputEventMouseButton:
 		pressed = event.is_pressed()
